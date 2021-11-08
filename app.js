f6b99c55eb0722f43fc3655633abf5326f2b6bcc
jadokao <jackykao528@outlook.com>
2021-11-08 22:12:50 +0800

    Add navbar to show login status

--- app.js
+++ app.js
@@ -16,14 +16,15 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
 app.use(flash())
 
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = req.user
   next()
 })
-
-app.use(passport.initialize())
-app.use(passport.session())
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
