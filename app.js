b88526323068fc75fa3956bb589e6bd75336e149
jadokao <jackykao528@outlook.com>
2021-11-09 01:48:39 +0800

    add Imgur API and dotenv

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const express = require('express')
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
