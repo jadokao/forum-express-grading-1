d791447045be6ba1df81be95957338950cba22db
jadokao <jackykao528@outlook.com>
2021-11-09 02:26:13 +0800

    [Assignment Preparation] ensureAuthenticated & getUser

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const flash = require('connect-flash')
 const session = require('express-session')
 const passport = require('./config/passport')
 const methodOverride = require('method-override')
+const helpers = require('./_helpers')
 const db = require('./models')
 const app = express()
 const port = process.env.PORT || 3000
@@ -31,7 +32,7 @@ app.use('/upload', express.static(__dirname + '/upload'))
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
-  res.locals.user = req.user
+  res.locals.user = helpers.getUser(req)
   next()
 })
 
