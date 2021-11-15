562616230400e6176ea30da4e6842c0def0125be
jadokao <jackykao528@outlook.com>
2021-11-16 00:02:41 +0800

    add id in users and restaurants seeder

--- seeders/20211108160833-users-seed-file.js
+++ seeders/20211108160833-users-seed-file.js
@@ -7,6 +7,7 @@ module.exports = {
       'Users',
       [
         {
+          id: 1,
           email: 'root@example.com',
           password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
           isAdmin: true,
@@ -15,6 +16,7 @@ module.exports = {
           updatedAt: new Date()
         },
         {
+          id: 2,
           email: 'user1@example.com',
           password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
           isAdmin: false,
@@ -23,6 +25,7 @@ module.exports = {
           updatedAt: new Date()
         },
         {
+          id: 3,
           email: 'user2@example.com',
           password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
           isAdmin: false,
