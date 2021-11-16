543e612f3e395a6e39526832d6ad403202428a71
jadokao <jackykao528@outlook.com>
2021-11-16 16:55:52 +0800

    complete Q2: 使用者可以 Like / Unlike 餐廳

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,6 +5,7 @@ const User = db.User
 const Comment = db.Comment
 const Restaurant = db.Restaurant
 const Favorite = db.Favorite
+const Like = db.Like
 const helpers = require('../_helpers')
 
 const userController = {
@@ -139,6 +140,24 @@ const userController = {
         return res.redirect('back')
       })
     })
+  },
+
+  addLike: async (req, res) => {
+    return Like.create({
+      UserId: helpers.getUser(req).id,
+      RestaurantId: req.params.restaurantId
+    }).then(like => {
+      return res.redirect('back')
+    })
+  },
+
+  removeLike: (req, res) => {
+    return Like.destroy({
+      where: {
+        UserId: helpers.getUser(req).id,
+        RestaurantId: req.params.restaurantId
+      }
+    }).then(like => res.redirect('back'))
   }
 }
 
