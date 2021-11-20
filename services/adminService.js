93f1fca69f3437e2dcb2a8cbe1811af658e369c0
jadokao <jackykao528@outlook.com>
2021-11-20 18:20:36 +0800

    complete GET /api/admin/restaurants/:id - 後台瀏覽個別餐廳

--- services/adminService.js
+++ services/adminService.js
@@ -7,6 +7,12 @@ const adminController = {
     return Restaurant.findAll({ raw: true, nest: true, include: [Category] }).then(restaurants => {
       callback({ restaurants })
     })
+  },
+
+  getRestaurant: (req, res, callback) => {
+    return Restaurant.findByPk(req.params.id, { include: [Category] }).then(restaurant => {
+      return callback({ restaurant: restaurant.toJSON() })
+    })
   }
 }
 
