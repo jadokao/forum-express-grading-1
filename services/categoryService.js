4ce479f9a09e2f84541d8bf737aa416cc05b7572
jadokao <jackykao528@outlook.com>
2021-11-20 23:25:51 +0800

    complete POST /api/admin/categories - 後台新增類別

--- services/categoryService.js
+++ services/categoryService.js
@@ -18,6 +18,16 @@ const categoryController = {
         return callback({ categories: categories })
       }
     })
+  },
+
+  postCategory: (req, res, callback) => {
+    if (!req.body.name) {
+      return callback({ status: 'error', message: "name didn't exist" })
+    } else {
+      return Category.create({ name: req.body.name }).then(category => {
+        return callback({ status: 'success', message: '' })
+      })
+    }
   }
 }
 
